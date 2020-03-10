af8516b914014d6f1ffad09b113aabbbfe0e8c5b
gelmonmartinez <60948268+gelmonmartinez@users.noreply.github.com>
2020-03-10 11:36:25 -0600

    Add files via upload

---
+++ src/metodosnumericosu2/MetodosNumericosU2.java
@@ -0,0 +1,11 @@
+package metodosnumericosu2;
+
+public class MetodosNumericosU2 {
+
+    public static void main(String[] args) {
+        frmPrincipal ventana = new frmPrincipal();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+}
